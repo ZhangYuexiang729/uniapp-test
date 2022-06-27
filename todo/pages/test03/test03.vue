4d706c643f50957e3fd0fa236ffa6ad9c98f4b28
ZhangYueXiang729 <1759886198@qq.com>
2022-06-27 10:32:37 +0800

    第一次修改

---
+++ todo/pages/test03/test03.vue
@@ -0,0 +1,27 @@
+<template>
+	<view>
+		test03
+		<button @click="open">跳转到text02</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			open(){
+				uni.switchTab({
+					url: '/pages/test02/test02'
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ todo/pages/text01/text01.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+		{{text.id}}
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				text:{}
+			}
+		},
+		onLoad(e) {
+			console.log("页面加载",e)
+			this.text = e
+		},
+		methods: {
+		
+		}
+	}
+</script>
+
+<style>
+
+</style>
